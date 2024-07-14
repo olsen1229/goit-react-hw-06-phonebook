7f3c5c416b901c76de2c108cba1a31057805d3a6
olsen1229 <marylou.olsen7@gmail.com>
2024-07-14 22:55:31 +0800

    createSlice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,92 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact, deleteContact } from '../redux/contactsSlice';
+import { setFilter } from '../redux/filterSlice';
+import { getContacts, getFilter } from '../redux/selectors';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-
-
-const retrievedInitialContacts = () => {
-  const savedContacts = JSON.parse(localStorage.getItem('contacts'));
-  //console.log('Retrieved contacts from localStorage: ', savedContacts);
-  // console.log('Retrieved contacts length from localStorage:', savedContacts.length);
-
-  if (savedContacts && savedContacts.length > 0) {
-    //console.log('Using saved contacts from localStorage');
-    return savedContacts;
-  }
-  console.log('Using default contacts');
-  return [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-
-  ];
-};
-
 export const App = () => {
-  const [contacts, setContacts] = useState(retrievedInitialContacts);
-  const [filter, setFilter] = useState('');
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
 
-  //console.log('contacts state value: ', contacts);
-
-  useEffect(() => {
-    //console.log('Saving contacts to localStorage:', contacts);
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = newContact => {
-    //console.log'Adding new contact:', newContact);
-    const duplicateContact = contacts.find(
-      contact => contact.name === newContact.name
-    );
-
-    if (duplicateContact) {
-      alert(`${newContact.name} is already in your contacts.`);
-      return;
-    }
-
-    
-    setContacts(prevContacts => {
-      const updatedContacts = [...prevContacts, newContact];
-      //console.log('Updated contacts after adding:', updatedContacts);
-      return updatedContacts;
-    });
+  const handleAddContact = newContact => {
+    // Placeholder for future Redux action
+    dispatch(addContact(newContact));
   };
 
 
-  const deleteContact = id => {
-    //console.log('Deleting contact with id:'m id);  
-    setContacts(prevContacts => {
-      const updatedContacts = prevContacts.filter(contact => contact.id !== id);
-    //console.log('Updaed contacts after deleting:', updatedContacts);
-      return updatedContacts; 
-
-    });
+ const handleDeleteContact = id => {
+    // Placeholder for future Redux action
+    dispatch(deleteContact(id));
   };
 
-  const filterContact = () => {
-    //console.log('Filtering contacts with filter:', filter);
-    const filterLowerCase = filter.toLowerCase();
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filterLowerCase)
-      
-    );
-    //console.log('Filtered contacts:', filteredContacts);
-    return filteredContacts;
+   const handleSetFilter = newFilter => {
+    // Placeholder for future Redux dispatch to update filter
+    dispatch(setFilter(newFilter));
   };
+
+  // Calculate filtered contacts directly within the App component
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
  
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm addContact={addContact} contacts={contacts}/>
+        <ContactForm addContact={handleAddContact} contacts={contacts}/>
 
         <h2>Contacts</h2>
-        <Filter filter={filter} setFilter={setFilter} />
+        <Filter filter={filter} setFilter={handleSetFilter} />
         <ContactList
-          filterContact={filterContact}
-          deleteContact={deleteContact} />
+          contacts={filteredContacts} // Passing the filteredContacts as prop
+          deleteContact={handleDeleteContact}
+        />
       </div>
     );
   };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,30 @@
+import React from 'react';
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 import PropTypes from 'prop-types';
+import css from './ContactList.module.css';
 
-export const ContactList = ({ filterContact, deleteContact }) => {
-    const filteredContacts = filterContact();
 
+export const ContactList = ({ contacts, deleteContact }) => {
     return (
-        <ul>
-            {filteredContacts.map(filteredContact => (
-                <ContactListItem
-                    key={filteredContact.id}
-                    filteredContact={filteredContact}
-                    deleteContact={deleteContact}
-                />
-            ))}
-        </ul>
-    );
+         <ul className={css.ulBox}>
+        {contacts.map(contact => (
+        <ContactListItem
+          key={contact.id}
+          filteredContact={contact}
+          deleteContact={deleteContact}
+        />
+      ))}
+    </ul>
+  );
 };
 
 ContactList.propTypes = {
-    filterContact: PropTypes.func.isRequired,
-    deleteContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  deleteContact: PropTypes.func.isRequired,
 };
